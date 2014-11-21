2bb74e7f09bb2b9295a64ba379dce2310c72b04e
nilrogen <nilrogen@gmail.com>
2014-11-21 15:23:11 -0500

    Cleaned up DownloadImages.py script

--- DownloadFiles.py
+++ DownloadFiles.py
@@ -1,3 +1,11 @@
+# AUTHOR: Michael Gorlin
+# DATE:   2014-11-21
+# 
+# Downloads hearthstone picture art from the all-cards.json file. 
+# This file was found at: 
+#   https://github.com/pdyck/hearthstone-db
+#
+
 import json
 import os
 import urllib2
@@ -8,22 +16,28 @@ JSONPATH = os.path.join(CURDIR, 'all-cards.json')
 PICSPATH = os.path.join(CURDIR,'pics' + os.sep)
 
 def loadJson():
+    """ Loads the JSON file and reads it into a map. """
     json_data = open(JSONPATH).read()
     pjson = json.loads(json_data)
     return pjson
 
 def downloadCard(card):
+    # remove bad space, colon, and apostrophy from card name. 
     cname = filter(lambda c: c not in ' :\'', card["name"])
     cname += '.png'
+
     curl  = card["image_url"]
 
+    # Ignore cards that have already been downloaded 
     os.chdir(PICSPATH)
     if cname in glob.glob('*.png'):
         return
     os.chdir("..")
     
+
     urldata = urllib2.urlopen(curl)
     cardpath = os.path.join(PICSPATH, cname)
+
     cardfile = open(cardpath, 'wb')
 
     print "Downloading: %s" % (cname)
@@ -35,13 +49,15 @@ def downloadCard(card):
 
 if __name__ == '__main__':
     try:
+        # Create pics directory if one does not exist
         os.mkdir(PICSPATH)
     except OSError:
         print 'Space Created'
         pass
     
-    json = loadJson()
 
+    # Download the cards
+    json = loadJson()
     cards = json["cards"]
     for card in cards:
         downloadCard(card)
