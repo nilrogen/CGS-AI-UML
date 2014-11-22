15d91acb252947ca984d3a6bf0c47e52da1d403c
nilrogen <nilrogen@gmail.com>
2014-11-22 01:53:47 -0500

    Continued my learning.

--- Testpygame.py
+++ Testpygame.py
@@ -11,11 +11,11 @@ class Application:
     def __init__(self):
         self.running = True
         self.surface_display = None
-        self.size = self.weight, self.height = 400, 600
+        self.size = self.weight, self.height = 500, 600
 
     def on_init(self):
         pygame.init()
-        self.surface_display = pygame.display.set_mode(self.size, pygame.HWSURFACE)
+        self.surface_display = pygame.display.set_mode(self.size, pygame.DOUBLEBUF, 32)
         self.running = True
 
 
@@ -59,11 +59,12 @@ class ImageApp(Application):
         if len(self.pathlist) == 0:
             pass
         Application.on_init(self)
-        self.images = [pygame.image.load(img).convert() for img in self.pathlist]
+        self.images = [pygame.image.load(img).convert_alpha() for img in self.pathlist]
 
         img = self.images[0]
-
-        self.images = map(lambda img: pygame.transform.smoothscale(img, (400, 600)), \
+        
+        aspw, asph = 200 * 1.5, 300 * 1.5
+        self.images = map(lambda img: pygame.transform.smoothscale(img, (int(aspw), int(asph))), \
                           self.images)
         self.currentImage = self.images[self.currentImageIndex]
 
@@ -74,21 +75,24 @@ class ImageApp(Application):
     def on_keydown(self):
         key = pygame.key.get_pressed()
         
-        if key[K_LEFT]:
+        if key[K_LEFT] or key[K_a]:
             self.currentImageIndex -= 1
             if self.currentImageIndex < 0:
                 self.currentImageIndex += len(self.pathlist)
             self.currentImage = self.images[self.currentImageIndex]
-        elif key[K_RIGHT]:
+        elif key[K_RIGHT] or key[K_d]:
             self.currentImageIndex += 1
             self.currentImageIndex %= len(self.pathlist)
             self.currentImage = self.images[self.currentImageIndex]
-        elif key[K_ESCAPE]:
+        elif key[K_ESCAPE] or key[K_q]:
             self.running = False
 
     def on_render(self):
-        self.surface_display.fill((0, 0, 0), pygame.Rect((0,0, 300, 500)))
-        self.surface_display.blit(self.currentImage, (0, 0))
+        #self.surface_display.fill((255, 255, 0), pygame.Rect((0, 0, 400, 600)))
+        self.surface_display.blit(self.images[self.currentImageIndex - 1], (0, 100))
+        self.surface_display.blit(self.images[ \
+                                  (self.currentImageIndex + 1) % len(self.images)], (200, 100))
+        self.surface_display.blit(self.currentImage, (100, 0))
         pygame.display.flip()
     
 if __name__ == '__main__':
