e4157e7410b28d35de0ef0996b3c568e604f2a50
nilrogen <nilrogen@gmail.com>
2014-11-23 16:16:04 -0500

    Commented some stuff and merged some files.

--- ui/uiobjects.py
+++ ui/uiobjects.py
@@ -1,11 +1,22 @@
 import pygame
 from pygame.locals import *
 
-class UIObject:
-    def __init__(self, parentsurface, boundingbox):
-        self.parentsurface = parentsurface
+import util.utilities as util
+
+class UIObject(object):
+
+    def __init__(self, parent, boundingbox):
+        self.parent = parent
         self.bb = boundingbox
-        self.pos = (self.bb.x self.bb.y)
+
+        self.children = None
+        self.surface = None
+
+        self.pos = (self.bb.x, self.bb.y)
+        self.x, self.y = self.bb.x, self.bb.y
+
+        if self.parent:
+            self.parent.addChild(self)
 
     def draw(self):
         pass
@@ -15,3 +26,29 @@ class UIObject:
 
     def getBoundingBox(self):
         return self.bb
+
+
+class UICard(UIObject):
+    def __init__(self, parent, card, boundingbox):
+        uiobject.__init__(self, parent, boundingbox)
+        self.card = card
+        self.image = util.getImage(card.getImageName())
+
+    def draw(self):
+        self.parent.surface.blit(self.image, self.pos)
+
+class Card(object):
+    """ Placeholder """
+    def __init__(self, name):
+        self.name = name
+        self.imagename = filter(lambda c: c not in ' :\'', card["name"])
+        self.imagename += '.png'
+
+    def getImageName(self):
+        return self.imagename
+
+
+
+
+
+
