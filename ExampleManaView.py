7a104ffc7a2964351a247c3b2112f16434aec1e8
nilrogen <nilrogen@gmail.com>
2014-12-03 12:57:07 -0500

    Added Images and fixed Path

---
+++ ExampleManaView.py
@@ -0,0 +1,39 @@
+import pygame
+from pygame.locals import *
+
+import util.utilities as util
+from ui.application import * 
+from ui.uiobjects import *
+from ui.mana import *
+
+class ManaApp(Application):
+    def __init__(self, windowsize=(515, 80)):
+        super(ManaApp, self).__init__(windowsize)
+        self.manaregion = ManaRegion(pygame.Rect(0, 0, 505, 55), 
+                                     'ManaFull.png', 
+                                     'ManaDepleted.png', 0)
+        
+    def onKeydown(self, event):
+        key = event.key
+        if key == K_a:
+            self.manaregion.useMana()
+        elif key == K_d:
+            self.manaregion.addMana()
+        elif key == K_w:
+            self.manaregion.newTurn(4)
+        elif key == K_t:
+            self.manaregion.newTurn()
+        elif key == K_r:
+            self.manaregion.resetMana()
+        elif key == K_q:
+            self.running = False
+            
+    def render(self):
+        self.manaregion.draw(self._display)
+        pygame.display.flip()
+
+ma = ManaApp()
+ma.execute()
+
+
+
